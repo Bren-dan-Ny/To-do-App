a0ef384edab64c58ddc515c8777593cbeeb4976f
Lhuana <lhuanaclopez14@gmail.com>
2025-10-18 12:32:01 -0500

    add image

--- to-do/src/components/TaskStats.jsx
+++ to-do/src/components/TaskStats.jsx
@@ -20,9 +20,34 @@ function TaskStats({ tasks = [] }) {
             <span>{pending}</span>
           </div>
         </div>
-        <div className="stat-card card-total">
-          <h4>Total de Tareas</h4>
-          <span>{total}</span>
+        <div className="container-stats-total-users">
+          <div className="stat-card card-total">
+            <h4>Total de Tareas</h4>
+            <span>{total}</span>
+          </div>
+          <div className="active-users">
+            <p>
+              <strong>25K+</strong>Usuarios Activos
+            </p>
+            <div className="avatars">
+              <img
+                src="https://randomuser.me/api/portraits/men/62.jpg"
+                alt=""
+              />
+              <img
+                src="https://randomuser.me/api/portraits/men/62.jpg"
+                alt=""
+              />
+              <img
+                src="https://randomuser.me/api/portraits/women/94.jpg"
+                alt=""
+              />
+              <img
+                src="https://randomuser.me/api/portraits/men/40.jpg"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </section>
     </>
